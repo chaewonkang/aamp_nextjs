d2dd3891d38b32538a00d1d05eef833108057a28
Chaewon Kang <saturday.the17th@gmail.com>
2021-10-21 16:28:46 +0900

    HotFix

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,12 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Serif+KR&display=swap'
+          rel='stylesheet'
+        />
         <title>AAMP - Becoming-Local</title>
       </Head>
       <Component {...pageProps} />
